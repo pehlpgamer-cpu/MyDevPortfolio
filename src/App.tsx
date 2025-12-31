3304eb8e8467a2ae6f9bedf1941dbf0ee1df30c6
Paulo <pehlpgamer@gmail.com>
2025-12-31 19:21:13 +0800

    Add new SVG and PNG assets for social media and database icons
    - Added gigachad-1.png and gigachad-2.png images to assets.
    - Introduced SVG files for GitHub, Gmail, LinkedIn, MySQL, Redis, and YouTube.
    - Removed outdated MySQL logo SVG asset.
    - Added new MySQL SVG logo to replace the removed asset.

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import { FaMapLocationDot } from "react-icons/fa6";
 import reactSvg from './assets/react-svgrepo-com.svg'
 import tsSvg from './assets/typescript-icon-svgrepo-com.svg'
 import laravelSvg from './assets/laravel-svgrepo-com.svg'
-import mysqlSvg from './assets/mysql-logo-svgrepo-com.svg'
+import mysqlSvg from './assets/mysql-svgrepo-com.svg'
 import sqliteSvg from './assets/sqlite-svgrepo-com.svg'
 import tailwindSvg from './assets/tailwind-svgrepo-com.svg'
+
+import pfp1 from './assets/gigachad-1.png'
 
 import './App.css'
 
@@ -17,17 +19,17 @@ function App() {
       </nav>
 
       <header>
-        <img className="h-16 w-16" src={reactSvg}/>
+        <img className="size-68 rounded-[9000px]" src={pfp1}/>
         <article>
           <h1 className='text-6xl'>Paulo E. Heymann</h1>
-          <h3 className="text-2xl">Full Stack Dev</h3>
+          <h3 className="text-2xl">22 year old 2nd year student & soon to be fullstack Dev, although I much more of a backend guy.</h3>
           <span className="flex gap-2 items-center">
             <FaMapLocationDot className="text-red-500 text-2xl"/><label className="text-2xl">Philippines, Mindoro</label>
           </span>
           {/*Tech Stack*/}
-          <div className="flex gap-2 max-h-16 max-w-16 mt-2">
+          <div className="flex gap-2 size-16 mt-2">
             <img className="" src={reactSvg} />
-            <img className="" src={tsSvg} />
+            <img className="rounded-lg" src={tsSvg} />
             <img className="" src={laravelSvg} />
             <img className="" src={tailwindSvg} />
             <img className="" src={mysqlSvg} />
